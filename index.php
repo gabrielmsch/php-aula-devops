8723c28b7169a822de2c9dabdd36889b19559951
Gabriel <gabri.schaefer@gmail.com>
2021-01-18 14:55:03 -0300

    versionando meu primeiro código PHP

--- index.php
+++ index.php
@@ -82,9 +82,8 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
   <div class="w3-content w3-justify w3-text-grey w3-padding-32" id="about">
     <h2>About</h2>
     <hr class="w3-opacity">
-    <p>Some text about me. Some text about me. I am lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing
-      elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    <p>Isabella é aquela criança enviada por Deus para fazer o bem desde cedo. 
+    Seguindo os passos de seus pais, criamos esta comunidade que tem o objetivo de arrecadar todo tipo de ajuda áquelas mais necessitadas, as crianças do continente Africano.
     </p>
     <h3 class="w3-padding-16">My Skills</h3>
     <p class="w3-wide">Photography</p>
